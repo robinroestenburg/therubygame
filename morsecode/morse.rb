55a2900aa4c5a522e38bb586dc1de452c1484d16
Robin Roestenburg <robinroestenburg@gmail.com>
2012-04-12 22:57:00 +0200

    Replaced hash by slower zip.

--- morsecode/morse.rb
+++ morsecode/morse.rb
@@ -1,5 +1,5 @@
 def morse_to_eng(morse)
-  morse_alphabet = ['.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'} 
+  morse_alphabet = Hash[%w(.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..).zip 'A'..'Z']
   morse.split(/\s{3}/).inject("") do |translation, morse_word|
     morse_word.split(/\s/).inject(translation) do |translation, morse_code|
       translation << morse_alphabet[morse_code]
